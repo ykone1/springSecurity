dc41add805194ba04d77554213c1263bb3b2401c
yukemeng <2869432967@qq.com>
2022-08-09 16:05:11 +0800

    测试GIT

---
+++ src/main/java/com/sangeng/security/git/TestGit.java
@@ -0,0 +1,12 @@
+package com.sangeng.security.git;
+
+/**
+ * @Auther:yukemeng Date:2022/8/9-08-09-16:01
+ * Description:演示git冲突
+ * version:1.0
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("==================1");
+    }
+}
