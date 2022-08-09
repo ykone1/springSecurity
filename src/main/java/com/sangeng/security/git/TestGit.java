e59ce437a4a2279c82fd1973767a6ac451d38783
yukemeng <2869432967@qq.com>
2022-08-09 16:09:57 +0800

    分支处理

--- src/main/java/com/sangeng/security/git/TestGit.java
+++ src/main/java/com/sangeng/security/git/TestGit.java
@@ -7,6 +7,7 @@ package com.sangeng.security.git;
  */
 public class TestGit {
     public static void main(String[] args) {
+        System.out.println("SpringSecurity分支");
         System.out.println("==================1");
     }
 }
